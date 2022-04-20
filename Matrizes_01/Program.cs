e78c34c03427b4fae52e436cc6a9b8a64dd72151
ricardopg8 <50276326+ricardopg8@users.noreply.github.com>
2022-04-19 22:02:11 -0300

    Matrizes_01
    Matrizes_01

---
+++ Matrizes_01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Matrizes_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Matrizes são bidimencionais:
+
+            int M, N;
+            int[,] A;
+
+            string[] s1 = Console.ReadLine().Split(' ');
+            M = int.Parse(s1[0]);
+            N = int.Parse(s1[1]);
+            A = new int[M, N];
+
+            for (int i=0; i<M; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                for (int j=0; j<N; j++)
+                {
+                    A[i, j] = int.Parse(s[j]);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
